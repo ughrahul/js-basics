b91a0c3bc35fd50313863370f3fb2f755d663877
rr <jhu>
2023-12-19 17:51:14 +0545

    learning forward to different variable

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ console.log(lan1);
 //This is also the new version ES6
 const lan2 = "I am also new buddy";
 console.log(lan2);
+
+//Programmer Name
+const p1 = "Rahul Kumar";
+var p2 = " Mandal";
+p3 = p1 + p2;
+console.log(p3);
+
+var num1 = "6";
+let num2 = "9";
+console.log(num1 + num2);
+
+let num3 = 6;
+const num4 = 9;
+console.log(num3 % num4);
+
+const pi = 3.14;
+let radius = 4.5;
+let radius1 = 6.9;
+console.log(2 * pi * radius1);
