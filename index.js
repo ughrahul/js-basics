4a37e7f409fa29d055f00123df1641ab65180d8c
rr <jhu>
2023-12-19 17:30:53 +0545

    learning about variables

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hey This is java script programming language");
+
+//This is older version ES5
+var lan = "Old_language";
+console.log(lan);
+
+//This is new version ES6
+let lan1 = "New_language";
+console.log(lan1);
+
+//This is also the new version ES6
+const lan2 = "I am also new buddy";
+console.log(lan2);
